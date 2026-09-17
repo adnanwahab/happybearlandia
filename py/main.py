560778c62e10896bf8444ad87ffca00ea71a4e35
adnan wahab <shel.bernstein123@gmail.com>
2026-09-17 09:40:55 -0500

    update file

--- py/main.py
+++ py/main.py
@@ -5,11 +5,14 @@ HandLandmarker = mp.tasks.vision.HandLandmarker
 HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
 VisionRunningMode = mp.tasks.vision.RunningMode
 
-# Create a hand landmarker instance with the image mode:
+# Create a hand landmarker instance forcing CPU execution
 options = HandLandmarkerOptions(
-    base_options=BaseOptions(model_asset_path='./hand_landmarker.task'),
-    running_mode=VisionRunningMode.IMAGE)
+    base_options=BaseOptions(
+        model_asset_path='./hand_landmarker.task',
+        delegate=BaseOptions.Delegate.CPU  # <-- This bypasses the Metal GPU crash
+    ),
+    running_mode=VisionRunningMode.IMAGE
+)
+
 with HandLandmarker.create_from_options(options) as landmarker:
-  # The landmarker is initialized. Use it here.
-  # ...
     print('123')
